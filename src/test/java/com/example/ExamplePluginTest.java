c438caae8f9894d0e105a35de05d48a0c9013626
nKeee <nikloskettunen@gmail.com>
2023-07-03 13:20:36 +0300

    NPCIndicator + Screenmarker test

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -1,4 +1,6 @@
 package com.example;
+import nKesNpchighlight.nKesNpcIndicatorsPlugin;
+import nKesScreenmarkers.nKesScreenMarkerPlugin;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
@@ -7,7 +9,12 @@ public class ExamplePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		//Testing only, only one plugin can be loaded this way
+
+		//ExternalPluginManager.loadBuiltin(nKesNpcIndicatorsPlugin.class);
+		//ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		//ExternalPluginManager.loadBuiltin(nKesScreenMarkerPlugin.class);
+
 		RuneLite.main(args);
 	}
 }
